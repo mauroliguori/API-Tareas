77128e37577c8c0d6ee6a03e5dae0cca05e9aff5
root <root@tareas.tareas-namespace.com>
2023-11-19 17:16:36 -0500

    Controller tareas

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tarea;
 
 class TareaController extends Controller
 {
-    //
+    public function List(Request $request){
+        return $tarea = Tarea::all();
+    }
+
+    public function Find(Request $request, $idTarea){
+        return $tarea = Tarea::FindOrFail($idTarea);
+    }
+
+    public function Delete(Request $request, $idTarea){
+        $tarea = Tarea::FindOrFail($idTarea);
+        $tarea -> delete();
+        return [ "message" => "Deleted"];
+    }
+
+
+    public function Update(Request $request, $idTarea){
+        $tarea = Tarea::FindOrFail($idTarea);
+        $tarea -> nombre = $request -> post("nombre");
+        $tarea -> tipo = $request -> post("tipo");
+        $tarea -> precio = $request -> post("precio");
+        $tarea -> gramos = $request -> post("gramos");
+        $tarea -> save();
+
+        return $tarea;
+    }
+
+    public function Create(Request $request){
+        $tarea = new tarea();
+        $tarea -> nombre = $request -> post("nombre");
+        $tarea -> tipo = $request -> post("tipo");
+        $tarea -> precio = $request -> post("precio");
+        $tarea -> gramos = $request -> post("gramos");
+
+        $tarea -> save();
+
+        return $tarea;
+    }
 }
